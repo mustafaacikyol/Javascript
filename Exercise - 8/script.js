13362132e3ddae09cbee939b3246eb548335365f
Mustafa Açıkyol <95076715+mustafaacikyol@users.noreply.github.com>
2022-04-30 18:35:48 +0300

    Create script.js

---
+++ Exercise - 8/script.js
@@ -0,0 +1,7 @@
+console.log("0(dahil) ile 10(dahil) arasındaki sayıların kareleri : ");
+let sayac = 0; 
+while ( sayac < 11 ) { 
+   let kare = sayac*sayac; 
+   console.log(kare); 
+   sayac++; 
+}
