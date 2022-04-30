da29890387dea229198e7a2d6b68e283ecc1c961
Mustafa Açıkyol <95076715+mustafaacikyol@users.noreply.github.com>
2022-04-30 19:21:48 +0300

    Create script.js

---
+++ Exercise -27/script.js
@@ -0,0 +1,6 @@
+ let a = prompt ("Lütfen 1, 2 ve 3 rakamından bir tanesini giriniz");
+ if ( a == 1 || a == 2 || a == 3) {
+   location.href = "https://www.google.com";
+ } else {
+   alert ('Girilen sayı geçersiz');
+ }
