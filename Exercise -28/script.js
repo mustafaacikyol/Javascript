98c0fd028d977bac9c08b87bb5fed8e6a705be66
Mustafa Açıkyol <95076715+mustafaacikyol@users.noreply.github.com>
2022-04-30 19:28:13 +0300

    Update script.js

--- Exercise -28/script.js
+++ Exercise -28/script.js
@@ -1,6 +1,26 @@
- let a = prompt ("Lütfen 1, 2 ve 3 rakamından bir tanesini giriniz");
- if ( a == 1 || a == 2 || a == 3) {
-   location.href = "https://www.google.com";
- } else {
-   alert ('Girilen sayı geçersiz');
- }
+let a =  prompt ( "Lütfen bir sayı giriniz" );
+
+if ( a != null ) {
+var c = prompt ( "Lütfen +, -, *, / operatörlerinden birini giriniz" );
+  }
+
+if ( c != null ) {
+var b = prompt ( "Lütfen bir sayı giriniz" );
+}
+
+if ( c == "+" ) {
+  let d = parseInt ( a ) + parseInt ( b );
+  document.querySelector('p').innerHTML = a + c + b + "=" + d ;
+} ;
+if ( c == "-" ) {
+  let d = parseInt ( a ) - parseInt ( b );
+  document.querySelector('p').innerHTML = a + c + b + "=" + d ;
+} ;
+if ( c == "*" ) {
+  let d = parseInt ( a ) * parseInt ( b );
+  document.querySelector('p').innerHTML = a + c + b + "=" + d ;
+} ;
+if ( c == "/" ) {
+  let d = parseInt ( a ) / parseInt ( b );
+  document.querySelector('p').innerHTML = a + c + b + "=" + d ;
+} ;
